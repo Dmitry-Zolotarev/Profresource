fa64e59f24e3323f0cdcedd8fc422c0b4619194b
Dmitry-Zolotarev <dimazed04@gmail.com>
2025-07-05 22:56:05 +0700

    05.07.2025

--- main/models.py
+++ main/models.py
@@ -1,5 +1,46 @@
 from django.db import models
 
+class Страны(models.Model):
+    Название = models.CharField(max_length=100, unique=True)
+    Краткое_название = models.CharField(max_length=50, unique=True)
+    Телефонный_код = models.CharField(max_length=10)
+
+class Регионы(models.Model):
+    Код = models.CharField(max_length=10)
+    Название = models.CharField(max_length=100)
+    Страна = models.ForeignKey(Страны, on_delete=models.CASCADE)
+
+class Районы(models.Model):
+    Название = models.CharField(max_length=100)
+    Регион = models.ForeignKey(Регионы, on_delete=models.CASCADE)
+
+class Типы_населенных_пунктов(models.Model):
+    Название = models.CharField(max_length=100, unique=True)
+
+class Населенные_пункты(models.Model):
+    тип = models.ForeignKey(Типы_населенных_пунктов, on_delete=models.CASCADE)
+    Название = models.CharField(max_length=100)
+    Район = models.ForeignKey(Районы, on_delete=models.CASCADE)
+
+class Почтовые_индексы(models.Model):
+    Индекс = models.CharField(max_length=10, unique=False)
+
+class Типы_улиц(models.Model):
+    Название = models.CharField(max_length=100)
+
+class Названия_улиц(models.Model):
+    Тип_улицы = models.ForeignKey(Типы_улиц, on_delete=models.SET_NULL, null=True)
+    Название = models.CharField(max_length=100)
+
+class Улицы(models.Model):
+    Название = models.ForeignKey(Названия_улиц, on_delete=models.CASCADE)
+    Населенный_пункт = models.ForeignKey(Населенные_пункты, on_delete=models.SET_NULL, null=True, blank=True)
+    Почтовый_индекс = models.ForeignKey(Почтовые_индексы, on_delete=models.SET_NULL, null=True, blank=True)
+
+class Адреса(models.Model):
+    улица = models.ForeignKey(Улицы, on_delete=models.CASCADE)
+    Номер_дома = models.CharField(max_length=20)
+
 class Пол(models.Model):
     Название = models.CharField(max_length=20, unique=True)
 
@@ -15,6 +56,7 @@ class Организации(models.Model):
     ОГРН = models.CharField(max_length=13, blank=True, unique=True)
     Телефон = models.CharField(max_length=10, blank=True, null=True)
     Email = models.EmailField(blank=True, null=True)
+    Адрес = models.ForeignKey(Адреса, on_delete=models.SET_NULL, null=True)
 
 class Курсы(models.Model):
     Тип = models.ForeignKey(Типы_курсов, on_delete=models.CASCADE)
@@ -23,11 +65,6 @@ class Курсы(models.Model):
 
 class Группы(models.Model):
     Курс = models.ForeignKey(Курсы, on_delete=models.CASCADE)
-
-class Страны(models.Model):
-    Название = models.CharField(max_length=100, unique=True)
-    Краткое_название = models.CharField(max_length=50, unique=True)
-    Телефонный_код = models.CharField(max_length=10)
 
 class Слушатели(models.Model):
     Фамилия = models.CharField(max_length=100, null=False)
@@ -42,6 +79,7 @@ class Слушатели(models.Model):
     ИНН = models.CharField(max_length=12, blank=True, null=True)
     Телефон = models.CharField(max_length=10, blank=True, null=True)
     Email = models.EmailField(blank=True, null=True)
+    Адрес = models.ForeignKey(Адреса, on_delete=models.SET_NULL, null=True)
 
 class Статусы(models.Model):
     Название = models.CharField(max_length=50, null=False, default="", unique=True)
@@ -51,7 +89,6 @@ class Приказы(models.Model):
 
 class Протоколы(models.Model):
     Hash = models.IntegerField(null=True)
-
 
 class Человек_группа(models.Model):
     Слушатель = models.ForeignKey(Слушатели, on_delete=models.CASCADE)
@@ -71,3 +108,4 @@ class Материалы_курсов(models.Model):
 
 class Месяцы(models.Model):
     Название = models.CharField(max_length=8)
+
